77b46b5d91c25959f1d234b4515c3cda53707464
Tristan Döhl <tristan.doehl@holy-mail.de>
2022-09-16 16:43:49 +0200

    wip

--- src/ThreeDLine.tsx
+++ src/ThreeDLine.tsx
@@ -1,58 +1,147 @@
 import * as t from 'three';
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from './vendor/OrbitControls';
+import { Maybe } from './util/types';
 
 
 interface Point3 {
   x: number;
   y: number;
   z: number;
-  color: string;
+  order: number;
+}
+
+type Color = string;
+
+type BinId = number;
+interface ColorGradient {
+  from: Color;
+  to: Color;
+}
+
+interface Bin {
+  id: BinId;
+  points: Point3[];
+  color: Color;
+  colorGradient: Maybe<ColorGradient>;
 }
 
 interface ThreeDLineProps {
-  canvas: {
+  canvas?: {
     width?: number;
     height?: number;
   },
   points: Point3[];
 }
 
+
+interface BinStore {
+  bins: Bin[];
+  active: BinId;
+}
+
+interface CreateBinProps {
+  color?: Color;
+  colorGradient?: ColorGradient;
+}
+
+type CreateBinFn = (props: CreateBinProps) => void;
+
 export const ThreeDLine: (props: ThreeDLineProps) => any = ({canvas, points}) => {
   const {width, height} = canvas || {width: 500, height: 500};
 
-  const renderer = new t.WebGLRenderer();
-  renderer.setSize( width, height );
+  const binStore: BinStore = {
+    active: 0,
+    bins: []
+  }
 
-  const camera = new t.PerspectiveCamera( 45, width / height, 0.1, 20000 );
-  camera.position.set( 0, 0, 400 );
-  camera.lookAt( 0, 0, 0 );
+  let renderer;
+  let camera;
+  let controls;
+  let scene;
 
 
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.update();
+  const setup = () => {
+    renderer = new t.WebGLRenderer();
+    renderer.setSize(width, height);
 
-  const scene = new t.Scene();
-  const material = new t.LineBasicMaterial( { color: "#00ff00" } );
+    camera = new t.PerspectiveCamera(45, width / height, 0.1, 20000);
+    camera.position.set(0, 0, 400);
+    camera.lookAt(0, 0, 0);
 
-  const parsedPoints = points.map(({x, y, z}) => new t.Vector3(x, y, 10*z));
 
-  const geometry = new t.BufferGeometry().setFromPoints(parsedPoints);
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.update();
 
-  const line = new t.Line( geometry, material );
+    scene = new t.Scene();
+  }
 
-  scene.add(line);
+  const doRender = () => {
+    renderer.render(scene, camera);
+  }
 
-  renderer.render(scene, camera);
+
+
+  const makePoints = () => {
+    const material = new t.LineBasicMaterial({color: '#00ff00'});
+    const parsedPoints = points.map(({x, y, z}) => new t.Vector3(x, y, 10 * z));
+    const geometry = new t.BufferGeometry().setFromPoints(parsedPoints);
+    const line = new t.Line(geometry, material);
+
+
+    scene.add(line);
+    doRender();
+  }
+
 
 
   function animate() {
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
     // required if controls.enableDamping or controls.autoRotate are set to true
     controls.update();
-    renderer.render( scene, camera );
+
   }
-  animate();
 
-  return renderer.domElement;
+  const startAnimation = () => {
+    animate();
+  }
 
+
+  const newBinId = () => {
+    return (1 + Math.max(0, ...binStore.bins.map(({id}) => id)));
+  }
+
+  const createBin: CreateBinFn = ({color = null, colorGradient = null} = {}) => {
+    const newId = newBinId();
+    const newBin = {
+      id: newId,
+      color: color,
+      colorGradient: colorGradient,
+      points: []
+    };
+
+    if(color === null) {
+      if(colorGradient === null) {
+        newBin.color = "#00ff00"; // Lovely default green
+      } else {
+        newBin.color = colorGradient.from;
+      }
+    }
+
+    binStore.bins = [...binStore.bins, newBin ];
+    binStore.active = newId;
+
+  }
+
+  const getBins = () => {
+    return binStore;
+  }
+
+  return {
+    canvas: renderer?.domElement,
+    setup,
+    startAnimation,
+    createBin,
+    getBins,
+  }
 }
---
+++ src/__tests__/bins.spec.ts
@@ -0,0 +1,36 @@
+import { ThreeDLine } from '../ThreeDLine';
+
+
+describe('bin doing bin things', () => {
+  test('creating bins', () => {
+
+    const {createBin, getBins} = ThreeDLine({points: []});
+
+    expect(createBin).toBeDefined();
+    expect(getBins).toBeDefined();
+
+    const bins = getBins();
+    expect(bins).toHaveProperty('active', 0);
+    expect(bins).toHaveProperty('bins', []);
+
+    createBin();
+
+    const newBins = getBins();
+
+    expect(newBins).toHaveProperty('active', 1);
+    expect(newBins).toHaveProperty('bins', [
+      {color: '#00ff00', colorGradient: null, id: 1, points: []}
+    ]);
+
+    const cg = {from: '#000000', to: '#ffffff'};
+    createBin({colorGradient: cg, color: '#ff00ff'})
+
+    const twoBins = getBins();
+    expect(twoBins).toHaveProperty('active', 2);
+    expect(twoBins).toHaveProperty('bins', [
+      {color: '#00ff00', colorGradient: null, id: 1, points: []},
+      {color: '#ff00ff', colorGradient: cg, id: 2, points: []}
+    ]);
+
+  });
+});
